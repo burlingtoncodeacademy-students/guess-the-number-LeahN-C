e5bc0c4edd9494baa8aadb943a708397d149c102
Leah N-C <Lniemcav@gmail.com>
2021-11-09 18:09:16 -0500

    extend the guess range

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function ask(questionText) {
 /* ----------------Globals--------------- */
 
 let min = 1;
-let max = 100;
+let max;
 
 /* ---------------Function Block------------*/
 
@@ -20,47 +20,88 @@ function randomNum(min, max) {
   let range = max - min + 1;
   return min + Math.floor(Math.random() * range);
 }
-randomNum(1, 100);
 
 function smartGuess(min, max) {
-  return Math.floor((min + max) / 2)
+  return Math.floor((min + max) / 2);
 }
-
 
 start();
 
 async function start() {
   console.log(
-    "Let's play a game where you (human) make up a number between " +
-      min +
-      " " +
-      "and " +
-      max +
-      " , and I (computer) try to guess it."
+    "Let's play a game where you (human) think of a number, and I (computer) try to guess it."
   );
+  max = await ask(
+    "First, pick a number greater than 1 to be your maximum range. "
+  );
+  max = parseInt(max);
+  console.log(typeof max);
   let secretNumber = await ask(
-    "What is your secret number?\nI won't peek, I promise...\n"
+    "Now, choose a number between " +
+      min +
+      " and " +
+      max +
+      "." +
+      " \nWhat is your secret number? \nI won't peek, I promise...\n"
   );
   console.log("You entered: " + secretNumber);
   let compGuess = smartGuess(min, max);
   let answer = await ask("Is your number " + compGuess + "? ");
 
   if (answer === "y" || answer === "yes") {
-    console.log("Woohoo!! ");
-    process.exit();
+    console.log("Woohoo! I guessed it on the first try!! ");
   } else {
-    while (answer !== "y") {
+    while (answer !== "y" || answer !== "yes") {
       let highLow = await ask("Is it higher or lower? (h/l) ");
-      if (highLow === "h" || highLow === "higher") {
-        min = compGuess + 1
-        compGuess = smartGuess(min, max)
-      }
-      else if (highLow === "l" || highLow === "lower") {
-        max = compGuess - 1
-        compGuess = smartGuess(min, max)
+      if (highLow === "h") {
+        min = compGuess + 1;
+        compGuess = smartGuess(min, max);
+      } else if (highLow === "l") {
+        max = compGuess - 1;
+        compGuess = smartGuess(min, max);
+      } else if (highLow === "l" && highLow < min) {
+        console.log(
+          "You said your number is higher than " +
+            min +
+            ", so it can't also be lower than " +
+            (min + 1) +
+            "! "
+        );
+      } else if (highLow === "h" && highLow > max) {
+        console.log(
+          "You said your number is lower than " +
+            max +
+            ", so it can't also be higher than " +
+            (max - 1) +
+            "! "
+        );
+      } else if (answer === "y" || answer === "yes") {
       }
       answer = await ask("Is your number " + compGuess + "? ");
-    }
+    } console.log(
+    "Yay! I finally guessed it! \nIt took me " +
+      +" tries to guess your number. "
+    );
   }
-process.exit();
+
+  /* Play again */
+  /*
+    async function playAgain(callback) {
+
+    }
+    playAgain(start)
+
+    ???
+      await ask("Do you want to play again? ");
+      while (answer === "y" || answer === "yes") {
+        secretNumber = await ask("What is your new secret number? ");
+        console.log("You entered: " + secretNumber);
+
+
+      } else if (answer === "n" || answer === "no") {
+        process.exit();
+      }
+        */
+
+  process.exit();
 }
